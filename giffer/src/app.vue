2e1b50a9eb35a3b19bb51d57264218c8e82c0c37
pbrach <8545076+pbrach@users.noreply.github.com>
2024-07-08 18:49:04 +0200

    basic drop handler

--- giffer/src/app.vue
+++ giffer/src/app.vue
@@ -1,8 +1,41 @@
 <template>
-  <h1>💖 Hello World!</h1>
-  <p>Welcome to your Electron application.</p>
+  <div class="app-main">
+    <div class="drop-zone" v-on:drop="onDrop">Drop here</div>
+    <div class="display-area">Display</div>
+  </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 console.log('👋 This message is being logged by "App.vue", included via Vite');
+
+function onDrop(ev: DragEvent) {
+  ev.stopPropagation();
+  ev.preventDefault();
+  const file = ev.dataTransfer.files[0];
+
+  validate(file);
+}
+
+function validate(file: File) {
+  const ending = file.name.split(".")[-1];
+  if (ending !== "gif") {
+    throw Error("Wrong file type. Must be '*.gif'.");
+  }
+}
 </script>
+
+<style>
+.app-main {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.drop-zone {
+  height: 30%;
+  width: 90%;
+  border: 5px solid lightgray;
+  border-radius: 10px;
+}
+</style>
