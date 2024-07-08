791e6d35bd08eb8a74eb43557c345a72e739faf1
pbrach <8545076+pbrach@users.noreply.github.com>
2024-07-08 19:45:43 +0200

    default formatting and type fixes

--- src/packages/lib/src/stream.ts
+++ src/packages/lib/src/stream.ts
@@ -1,40 +1,39 @@
 export class SuperGifStream {
+  private position = 0;
 
-    private position = 0;
+  constructor(private data) {}
 
-    constructor(private data) {
+  readByte() {
+    if (this.position >= this.data.length) {
+      throw new Error("Attempted to read past end of stream.");
     }
 
-    readByte() {
-        if (this.position >= this.data.length) {
-            throw new Error('Attempted to read past end of stream.');
-        }
+    if (this.data instanceof Uint8Array) {
+      return this.data[this.position++];
+    } else {
+      return this.data.charCodeAt(this.position++) & 0xff;
+    }
+  }
 
-        if (this.data instanceof Uint8Array) {
-            return this.data[this.position++];
-        } else {
-            return this.data.charCodeAt(this.position++) & 0xFF;
-        }
-    };
+  readBytes(n) {
+    let bytes = [];
+    for (let i = 0; i < n; i++) {
+      bytes.push(this.readByte());
+    }
+    return bytes;
+  }
 
-    readBytes(n) {
-        let bytes = [];
-        for (let i = 0; i < n; i++) {
-            bytes.push(this.readByte());
-        }
-        return bytes;
-    };
+  read(n: number) {
+    let s = "";
+    for (let i = 0; i < n; i++) {
+      s += String.fromCharCode(this.readByte());
+    }
+    return s;
+  }
 
-    read(n: number) {
-        let s = '';
-        for (let i = 0; i < n; i++) {
-            s += String.fromCharCode(this.readByte());
-        }
-        return s;
-    };
-
-    readUnsigned() { // Little-endian.
-        let a = this.readBytes(2);
-        return (a[1] << 8) + a[0];
-    };
+  readUnsigned() {
+    // Little-endian.
+    let a = this.readBytes(2);
+    return (a[1] << 8) + a[0];
+  }
 }
